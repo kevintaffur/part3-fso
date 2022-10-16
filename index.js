d3bfb5abfaa16ec37689f914a3f133edb1472cb0
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-16 17:26:55 -0500

    exercise 3.10

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.post('/api/persons', (request, response) => {
   return response.json(person);
 });
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
