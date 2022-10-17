de972e94af6a0db00c0ae12bdb637a97236ef043
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-16 21:41:46 -0500

    exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://kevin:${password}@cluster0.7mnvd1t.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 3) {
+  console.log('phonebook:');
+  mongoose
+    .connect(url)
+    .then(() => {
+      Person
+        .find({})
+        .then(persons => {
+          persons.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+          });
+          mongoose.connection.close();
+        });
+    });
+  return;
+}
+
+mongoose
+  .connect(url)
+  .then(() => {
+    const person = new Person({
+      name: name,
+      number: number
+    });
+
+    return person.save();
+  })
+  .then(() => {
+    console.log(`added ${name} number ${number} to the phonebook`);
+    return mongoose.connection.close();
+  })
+  .catch((error) => console.log(error));
