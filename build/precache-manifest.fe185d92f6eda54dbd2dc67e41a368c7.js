1b0610ef0e99e961dc89b616ef7aff221c696520
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-18 08:18:46 -0500

    exercise 3.21

--- build/precache-manifest.fe185d92f6eda54dbd2dc67e41a368c7.js
+++ build/precache-manifest.fe185d92f6eda54dbd2dc67e41a368c7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ea37dbc7a0067a68bf4a",
+    "revision": "f8bc99fd3c888d103e11",
     "url": "/static/css/main.0086f07b.chunk.css"
   },
   {
-    "revision": "ea37dbc7a0067a68bf4a",
-    "url": "/static/js/main.dc7a67f4.chunk.js"
+    "revision": "f8bc99fd3c888d103e11",
+    "url": "/static/js/main.9eb61c07.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.6193b540.chunk.js"
   },
   {
-    "revision": "7b9a8ff35fd5be62d063108803b9563f",
+    "revision": "c759bcff7e30d841703d7de002ee7ce0",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7633e5a2f29aa5b2ae9d165aeca02ee3.js"
+  "/precache-manifest.fe185d92f6eda54dbd2dc67e41a368c7.js"
 );
 
 workbox.clientsClaim();
